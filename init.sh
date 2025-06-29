b01942f584574d1b96d7a6e94247dd5c19c3f063
Pavel Nuzhnou <pavelnuzhnou@Pavels-MacBook-Air.local>
2025-06-30 00:37:45 +0300

    fix:;2C;2C make init.sh visible and executable in Docker

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Fetching database from Dropbox."
+echo "Fetching database from Dropbox!"
 wget -O /home/node/.n8n/database.sqlite "https://dl.dropboxusercontent.com/scl/fi/e1lc8a52t6fv3d86mlwp1/database.sqlite?rlkey=t6t3941pudg4vp0p1h363dcgi&dl=1"
 echo "Done fetching database!"
 
