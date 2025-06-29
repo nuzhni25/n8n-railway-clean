084c8983e721c87d60538455bb83059150755fac
Pavel Nuzhnou <pavelnuzhnou@Pavels-MacBook-Air.local>
2025-06-30 00:07:04 +0300

    n8n setup on Railway with Dropbox db

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,4 @@ RUN apk add --no-cache wget
 COPY init.sh /init.sh
 RUN chmod +x /init.sh
 
-ENTRYPOINT ["/init.sh"]
-
+CMD ["/init.sh"]
--- init.sh
+++ init.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-echo "Fetching database from Dropbox."
-wget -O /data/database.sqlite "https://www.dropbox.com/scl/fi/e1lc8a52t6fv3d86mlwp1/database.sqlite?rlkey=t6t3941pudg4vp0p1h363dcgi&dl=1"
+echo "Fetching database from Dropbox..."
+wget -O /home/node/.n8n/database.sqlite "https://dl.dropboxusercontent.com/scl/fi/e1lc8a52t6fv3d86mlwp1/database.sqlite?rlkey=t6t3941pudg4vp0p1h363dcgi&dl=1"
 echo "Done fetching database!"
 
-exec /app/n8n
-
+exec n8n
