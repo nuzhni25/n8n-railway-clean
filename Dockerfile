dbbafbbc8b621dede52e4a305b52d20eb4a42f78
Pavel Nuzhnou <pavelnuzhnou@Pavels-MacBook-Air.local>
2025-06-29 23:54:54 +0300

    init: working Railway setup with Dropbox

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,8 @@ FROM n8nio/n8n:1.45.1
 
 USER root
 RUN apk add --no-cache wget
-
 COPY init.sh /init.sh
 RUN chmod +x /init.sh
 
 ENTRYPOINT ["/init.sh"]
-
 
--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo "Fetching database from Dropbox..."
-wget -O /data/database.sqlite "https://www.dropbox.com/scl/fi/e1lc8a52t6fv3d86mlwp1/database.sqlite?rlkey=t6t3941pudg4vp0p1h363dcgi&st=7ujvsjme&dl=1"
+echo "Fetching database from Dropbox."
+wget -O /data/database.sqlite "https://www.dropbox.com/scl/fi/e1lc8a52t6fv3d86mlwp1/database.sqlite?rlkey=t6t3941pudg4vp0p1h363dcgi&dl=1"
 echo "Done fetching database!"
 
 exec /app/n8n
