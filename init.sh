e287173e66918bd908f35d62bb1403926a9b6369
Pavel Nuzhnou <pavelnuzhnou@Pavels-MacBook-Air.local>
2025-06-29 22:44:07 +0300

    fix: run n8n after downloading database

--- init.sh
+++ init.sh
@@ -2,7 +2,8 @@
 
 echo "🔁 Fetching database from Dropbox..."
 wget -O /data/database.sqlite "https://dl.dropboxusercontent.com/scl/fi/e1lc8a52t6fv3d86mlwp1/database.sqlite?rlkey=t6t3941pudg4vp0p1h363dcgi&dl=1"
-
 echo "✅ Done fetching database!"
-exec tini -- /app/n8n "$@"
+
+# Запускаем n8n
+exec n8n
 
