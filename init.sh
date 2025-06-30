727ed016cc039cb7cb06a93641f56f72fd68164c
Pavel Nuzhnou <pavelnuzhnou@Pavels-MacBook-Air.local>
2025-06-30 03:32:26 +0300

    Clean: remove init.sh and use volume

--- init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Fetching database from Dropbox!"
-wget -O /home/node/.n8n/database.sqlite "https://dl.dropboxusercontent.com/scl/fi/e1lc8a52t6fv3d86mlwp1/database.sqlite?rlkey=t6t3941pudg4vp0p1h363dcgi&dl=1"
-echo "Done fetching database!"
-
-exec n8n
